d0c08b385b26dc775623b8a73f42f8e81f870016
Tommy Windows <mitrakov-artem@yandex.ru>
2025-02-12 18:11:06 +0300

    Add installer for Windows

--- windows/LasNotes/Program.cs
+++ windows/LasNotes/Program.cs
@@ -3,7 +3,7 @@ namespace LasNotes;
 // build:
 // 1) raise Version, AssemblyVersion and FileVersion in LasNotes.csproj
 // 2) run: dotnet publish --self-contained --output "Las Notes"
-// 3) compile installer.iss with InnoSetup (=> check Output folder)
+// 3) compile _installer/LasNotes.aip with Advanced Installer v20.9
 internal static class Program {
     [STAThread]
     internal static void Main() {
